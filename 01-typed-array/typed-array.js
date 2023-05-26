e97e0545cfd6a4deeb89114c4477b9f9ccc58caa
Miller <nmiller2379@gmail.com>
2023-05-26 19:27:24 -0400

    typed array working

---
+++ 01-typed-array/typed-array.js
@@ -0,0 +1,10 @@
+// Create a const buffer of 64 bytes
+
+//Create a const i32View 32-bit int typed array with the above buffer
+
+// Write your code above this line
+// ===================================================================
+
+// DO NOT ALTER CODE BELOW THIS LINE
+module.exports = buffer;
+module.exports = i32View;

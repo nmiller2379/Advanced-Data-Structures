c649d58898a57e65aa4e13cc5f05b9bdde6d9d5d
unknown <veronica.n.harris92@gmail.com>
2023-07-27 09:18:37 -0400

    added 02-Stack answers

---
+++ instructor-version/02-Stack/stack.js
@@ -0,0 +1,7 @@
+const homeworkStack = ["BIO12", "HIS80", "MAT122", "CS50"];
+// Your code goes here
+
+homeworkStack.push("PSY44");
+homeworkStack.pop()
+// DO NOT CHANGE THE CODE BELOW THIS LINE
+module.exports = homeworkStack;

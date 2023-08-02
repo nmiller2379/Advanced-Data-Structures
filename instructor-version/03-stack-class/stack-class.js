71fefdacc8c013d8e610cfd888c137e60f00b32d
unknown <veronica.n.harris92@gmail.com>
2023-08-02 14:40:23 -0400

    pausing on stack class

--- instructor-version/03-stack-class/stack-class.js
+++ instructor-version/03-stack-class/stack-class.js
@@ -4,6 +4,32 @@ class Stack {
   }
 
   //   Your code goes here.
+  push(e){
+    return this.collection.push(e)
+  }
+
+  pop(e) {
+    
+    return this.collection.pop(e)
+  }
+
+  peek(e) {
+    this.collection.filter((a, b) => a > b)
+    return this.collection.peek(e)
+    // return this.collection.push()
+  }
+
+  isEmpty(){
+    if (this.collection == 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  clear() {
+   
+  }
 
   print() {
     console.log(this.collection);

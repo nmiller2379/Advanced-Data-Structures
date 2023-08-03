816893b120385c89536b2e5baea84315081aee51
unknown <veronica.n.harris92@gmail.com>
2023-08-03 13:36:05 -0400

    finished up to circular-queue.js

--- instructor-version/06-circular-queue/circular-queue.js
+++ instructor-version/06-circular-queue/circular-queue.js
@@ -16,11 +16,29 @@ class CircularQueue {
     }
   
     enqueue(item) {
-    // Your code goes here   
+    // Your code goes here
+    if(this.queue[this.write] === null){
+      this.queue[this.write++] = item
+      if(this.write > this.max) {
+        this.write = 0;
+      }
+      return item;
+    } else {
+      return null;
+    }
     }
   
     dequeue() {
     // Your code goes here
+      if(this.queue[this.read] !== null) {
+        let item = this.queue[this.read]
+        this.queue[this.read++] = null
+        if(this.read > this.max) {
+          this.read = 0;
+        }
+        return item;
+      }
+      return null;
     }
   }
 

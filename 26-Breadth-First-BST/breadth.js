a811e16ed09685472ee32a83a73c5970b369d386
Miller <nmiller2379@gmail.com>
2024-03-02 13:51:33 -0500

    BST breadth done

--- 26-Breadth-First-BST/breadth.js
+++ 26-Breadth-First-BST/breadth.js
@@ -15,19 +15,5 @@ class BinarySearchTree {
   // Your code here
 }
 
-const bst = new BinarySearchTree();
-bst.root = new Node(15);
-bst.root.left = new Node(10);
-bst.root.right = new Node(20);
-bst.root.left.left = new Node(8);
-bst.root.left.right = new Node(12);
-bst.root.right.left = new Node(16);
-bst.root.right.right = new Node(25);
-
-displayTree(bst);
-
-console.log(bst.levelOrder());
-console.log(bst.reverseLevelOrder());
-
 // DO NOT CHANGE THE CODE BELOW THIS LINE
 module.exports = { Node, BinarySearchTree };
